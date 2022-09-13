9d15db7bb66b9dde30271be28ad5a85469d177cf
brunnaCellia <brunnacellia@gmail.com>
2022-09-12 22:32:23 -0300

    Classe cliente

--- superbeauty/src/main/java/br/com/unicv/superbeauty/model/Cliente.java
+++ superbeauty/src/main/java/br/com/unicv/superbeauty/model/Cliente.java
@@ -1,5 +1,56 @@
 package br.com.unicv.superbeauty.model;
 
-public class Cliente {
-    
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Entity
+@Table (name = "Cliente")
+public class Cliente implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codCliente;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "dataNascimento")
+    private LocalDate dataNascimento;
+
+    private Genero genero;
+
+    @Column(name = "celular")
+    private String celular;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "logradouro")
+    private String logradouro;
+
+    @Column(name = "bairro")
+    private String bairro;
+
+    @Column(name = "numero")
+    private String numero;
+
+    @Column(name = "cep")
+    private String cep;
 }
---
+++ superbeauty/src/main/java/br/com/unicv/superbeauty/model/Genero.java
@@ -0,0 +1,6 @@
+package br.com.unicv.superbeauty.model;
+
+public enum Genero {
+    MASCULINO,
+    FEMININO;
+}
