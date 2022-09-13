c86fafbfec671f156cc7883f21dba95c78f2449d
kassiazama <kassiazama@gmail.com>
2022-09-12 22:04:02 -0300

    primeiro commit

---
+++ superbeauty/src/main/java/br/com/unicv/superbeauty/SuperbeautyApplication.java
@@ -0,0 +1,13 @@
+package br.com.unicv.superbeauty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperbeautyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperbeautyApplication.class, args);
+	}
+
+}
---
+++ superbeauty/src/test/java/br/com/unicv/superbeauty/SuperbeautyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unicv.superbeauty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperbeautyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
