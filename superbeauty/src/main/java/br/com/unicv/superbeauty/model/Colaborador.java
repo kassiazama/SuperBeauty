c6d3de483d329f435e2919b149c5f776bce9b7b1
nataliaSantos <natalia.sntss@hotmail.com>
2022-09-12 22:26:22 -0300

    criado a classe Colaborador

---
+++ superbeauty/src/main/java/br/com/unicv/superbeauty/model/Colaborador.java
@@ -0,0 +1,45 @@
+package br.com.unicv.superbeauty.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+@Entity
+public class Colaborador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "codColaborador")
+    private Integer codColaborador;
+
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "dataNascimento")
+    private LocalDate dataNascimento;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "contato")
+    private String contato;
+
+    @Column(name = "dataContato")
+    private LocalDate dataContato;
+
+    @Column(name = "status")
+    private boolean  status;
+
+
+}
