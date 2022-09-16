f370b1e82aaa8bdcd3b78eca099686acc4fba31e
guic369 <guimestreyoda@outlook.com>
2022-09-15 22:26:42 -0300

    Att

---
+++ superbeauty/src/main/java/br/com/unicv/superbeauty/repository/ServicoRepository.java
@@ -0,0 +1,11 @@
+package br.com.unicv.superbeauty.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.unicv.superbeauty.model.Servico;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+
+    
+    
+}
---
+++ superbeauty/src/main/java/br/com/unicv/superbeauty/service/ServicoServicelmpl.java
@@ -0,0 +1,13 @@
+package br.com.unicv.superbeauty.service;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicoServicelmpl implements ServicoService{
+
+    
+
+    
+}
