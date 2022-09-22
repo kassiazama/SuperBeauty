7c02a89c2f0115898b0e5a630322566c5dc79f19
nataliasntss <natalia.sntss@hotmail.com>
2022-09-22 20:33:38 -0300

    implementado ColaboradorService e ColaboradorRepository

---
+++ superbeauty/src/main/java/br/com/unicv/superbeauty/service/ColaboradorService.java
@@ -0,0 +1,24 @@
+package br.com.unicv.superbeauty.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import br.com.unicv.superbeauty.model.Colaborador;
+
+@Component
+public interface ColaboradorService {
+  
+    public Colaborador cadastrar(Colaborador colaborador);
+
+    public Colaborador editar(Colaborador colaborador);
+
+    public List<Colaborador> listar();
+
+    public Colaborador buscarPorId(Integer codColaborador); 
+
+    public Colaborador buscarPorEmail(String email);
+    
+
+    public void excluir(Integer codColaborador);
+}
