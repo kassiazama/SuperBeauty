c9626af442db83bde1fd3369c58e5024b9a4f350
kassiazama <kassiazama@gmail.com>
2022-09-23 00:17:27 -0300

    correções

--- superbeauty/src/main/java/br/com/unicv/superbeauty/model/Agendamento.java
+++ superbeauty/src/main/java/br/com/unicv/superbeauty/model/Agendamento.java
@@ -1,7 +1,6 @@
 package br.com.unicv.superbeauty.model;
 
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
@@ -11,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,22 +29,24 @@ public class Agendamento implements Serializable {
     @Column(name = "codagendamento")
     private Integer codAgendamento;
     
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @Column(name = "datahora")
     private LocalDateTime dataHora;
     
     @ManyToOne
-    @JoinColumn(name = "codColaborador", referencedColumnName = "codcolaborador")
+    @JoinColumn(name = "codcolaborador", referencedColumnName = "codColaborador")
     private Colaborador colaborador;
 
     @ManyToOne
-    @JoinColumn(name = "codServico", referencedColumnName = "codservico")
+    @JoinColumn(name = "codservico", referencedColumnName = "codServico")
     private Servico servico;
     
     @ManyToOne
-    @JoinColumn(name = "codEmpresa", referencedColumnName = "codempresa")
+    @JoinColumn(name = "codempresa", referencedColumnName = "codEmpresa")
     private Empresa empresa;
     
     @ManyToOne
-    @JoinColumn(name = "codCliente", referencedColumnName = "codcliente")
+    @JoinColumn(name = "codcliente", referencedColumnName = "codCliente")
     private Cliente cliente;
     
 
